1a8a8b0d0e7393921d0a30a9d8798e04461f3206
Nischay <barejanischay5@gmail.com>
2024-03-05 02:27:49 +0530

    menu completed

--- menu.js
+++
@@ -1,7 +0,0 @@
-const preloader = document.querySelector("[data-preaload]");
-
-window.addEventListener("load", function () {
-    preloader.classList.add("loaded");
-    document.body.classList.add("loaded");
-});
-
